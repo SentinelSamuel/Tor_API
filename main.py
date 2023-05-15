9a7485a530dbcd204ece57856c8365719717d6e1
Samuel <114468569+SentinelSamuel@users.noreply.github.com>
2023-05-15 14:17:15 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Entry point."""
+
+import argparse
+from Tor_Project.api_tor import get_exit_nodes_ip
+
+# Parameters
+parser = argparse.ArgumentParser(description='Retrieves Tor exit nodes ip address using Tor DNSBL API')
+parser.add_argument('-E', '--export', action="store", type=str, required=False, help='Export results in your file')
+parser.add_argument('-s', '--silent' , action="store_true", required=False, help='Display nothing but the result')
+parser.add_argument('--debug', action="store_true", required=False, help='Get all info')
+args = parser.parse_args()
+
+def main():
+    '''
+        Does everything
+    '''
+    get_exit_nodes_ip(args.export, args.silent, args.debug)
+
+if __name__ == "__main__":
+    main()
